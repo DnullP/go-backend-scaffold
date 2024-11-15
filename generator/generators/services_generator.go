e29c07f1599e6afcda06c3bc4d8a591f2dae96ad
dnull_p <dnull123p@gmail.com>
2024-11-15 07:20:29 +0000

    modify: 将服务启动逻辑转移到模板外部

--- generator/generators/services_generator.go
+++ generator/generators/services_generator.go
@@ -106,103 +106,32 @@ package {{.PackageName}}
 
 import (
     "context"
-	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"strconv"
-
-	"go-backend-scaffold/config"
 	pb "go-backend-scaffold/proto"
-	"go-backend-scaffold/services/discovery"
-	"go-backend-scaffold/trace"
-
-	"github.com/google/uuid"
-	"github.com/hashicorp/consul/api"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 )
-
-
 // {{.ServiceName}}Server 是服务接口，用户需要实现这个接口
 type {{.ServiceName}}Server interface {
     {{range .Methods}}
     {{.MethodName}}(context.Context, *pb.{{.RequestType}}) (*pb.{{.ResponseType}}, error)
     {{end}}
 }
-
 // server 是 gRPC 服务器的实现
 type {{.ServiceName}}Implement struct {
     pb.Unimplemented{{.ServiceName}}Server
     Handler {{.ServiceName}}Server
 }
-
 {{range .Methods}}
 // {{.MethodName}} 实现了 {{$.ServiceName}} 的 {{.MethodName}} 方法
 func (s *{{$.ServiceName}}Implement) {{.MethodName}}(ctx context.Context, req *pb.{{.RequestType}}) (*pb.{{.ResponseType}}, error) {
     return s.Handler.{{.MethodName}}(ctx, req)
 }
 {{end}}
-
-
 // Start{{.ServiceName}}Server 启动 gRPC 服务器
 func Start{{.ServiceName}}Server(ctx context.Context, port string, handler {{.ServiceName}}Server) error {
-	//设置监听端口
-    lis, err := net.Listen("tcp", ":" + port)
-    if err != nil {
-        return fmt.Errorf("failed to listen: %v", err)
-    }
-
-	trace.SetTraceProvider("{{.ServiceName}}")
-
-	//创建grpc服务器
-    grpcServer := grpc.NewServer(grpc.StatsHandler(otelgrpc.NewServerHandler()))
-    pb.Register{{.ServiceName}}Server(grpcServer, &{{.ServiceName}}Implement{Handler: handler})
-
-	//创建健康检查服务
-	go func() {
-		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-			log.Println("收到健康检查请求")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("OK"))
-		})
-		if err := http.ListenAndServe(":8000", nil); err != nil {
-			log.Printf("无法启动健康检查 HTTP 服务器: %v", err)
-		}
-	}()
-
-	//注册服务
-	serverId := uuid.New().String()
-	iport, _ := strconv.Atoi(port)
-	err = discovery.RegisterService("{{.ServiceName}}", serverId, iport)
-	if err != nil {
-		log.Printf("服务注册失败")
-		panic(err)
-	}
-	log.Printf("服务 %s 注册成功", serverId)
-
-	//注销服务
-	consulConfig := api.DefaultConfig()
-	consulConfig.Address = config.Consul.Address + ":8500"
-	client, _ := api.NewClient(consulConfig)
-	defer func() {
-		err := discovery.DeregisterService(serverId, client)
-		if err != nil {
-			log.Printf("服务注销失败: %v", err)
-		} else {
-			log.Printf("服务 %s 已注销", serverId)
-		}
-	}()
-
-	//关闭服务
-	go func() {
-		<-ctx.Done()
-		log.Println("接收到关闭信号，正在优雅地停止 gRPC 服务器...")
-		grpcServer.GracefulStop()
-	}()
-
-    log.Printf("gRPC server {{.ServiceName}} is running on port :%s", port)
-    return grpcServer.Serve(lis)
+	grpcServer := grpc.NewServer(grpc.StatsHandler(otelgrpc.NewServerHandler()))
+	pb.Register{{.ServiceName}}Server(grpcServer, &{{.ServiceName}}Implement{Handler: handler})
+	return startServer(ctx, port, grpcServer, "{{.ServiceName}}")
 }
 `
 
---
+++ services/generated/start_server_.go
@@ -0,0 +1,70 @@
+package generated
+
+import (
+	"context"
+	"fmt"
+	"go-backend-scaffold/config"
+	"go-backend-scaffold/services/discovery"
+	"go-backend-scaffold/trace"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+
+	"github.com/google/uuid"
+	"github.com/hashicorp/consul/api"
+	"google.golang.org/grpc"
+)
+
+func startServer(ctx context.Context, port string, server *grpc.Server, serviceName string) error {
+	//设置监听端口
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}
+
+	trace.SetTraceProvider(serviceName)
+
+	//创建健康检查服务
+	go func() {
+		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+			log.Println("收到健康检查请求")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK"))
+		})
+		if err := http.ListenAndServe(":8000", nil); err != nil {
+			log.Printf("无法启动健康检查 HTTP 服务器: %v", err)
+		}
+	}()
+
+	//注册服务
+	serverId := uuid.New().String()
+	iport, _ := strconv.Atoi(port)
+	err = discovery.RegisterService(serviceName, serverId, iport)
+	if err != nil {
+		log.Printf("服务注册失败")
+		panic(err)
+	}
+	log.Printf("服务 %s 注册成功", serverId)
+
+	//注销服务
+	client, _ := api.NewClient(config.Consul)
+	defer func() {
+		err := discovery.DeregisterService(serverId, client)
+		if err != nil {
+			log.Printf("服务注销失败: %v", err)
+		} else {
+			log.Printf("服务 %s 已注销", serverId)
+		}
+	}()
+
+	//关闭服务
+	go func() {
+		<-ctx.Done()
+		log.Println("接收到关闭信号，正在优雅地停止 gRPC 服务器...")
+		server.GracefulStop()
+	}()
+
+	log.Printf("gRPC server %s is running on port :%s", serviceName, port)
+	return server.Serve(lis)
+}
